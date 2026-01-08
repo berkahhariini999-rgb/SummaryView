635d7096e8cce13b69f7b04d19fac24c6220b09f
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-01-08 09:31:11 +0700

    Initial Commit

---
+++ SummaryView/SummaryViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummaryViewApp.swift
+//  SummaryView
+//
+//  Created by Iqbal Alhadad on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SummaryViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
